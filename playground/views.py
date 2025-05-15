3f2f17350601d916708eef125e90a79d68315048
nour-gab <gaboussanour123@gmail.com>
2025-05-15 06:56:33 -0400

    djang_orm_part_1

--- playground/views.py
+++ playground/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
+from django.db.models import Q, F, Value
+from django.db.models.aggregates import Count, Max, Min, Avg
+from store.models import Product, OrderItem, Customer
 
 # Create your views here.
 
-def calculate():
-    x=1
-    return x
 
 
 def say_hello(request):
-    x=calculate()
-    return render(request, 'hello.html', {'name': 'Nour'})
+
+    #queryset = Product.objects.filter(Q(inventory__lt=10) | ~Q(price__lt=20))
+    #queryset = Product.objects.filter(collection__id=3).order_by('price')[:5].values_list('id' ,'title' ,'collection__title')
+    #select_related --1 instance || prefetch_related
+    #queryset=Product.objects.prefetch_related('promotions').all()
+    #result = Product.objects.aggregate(count= Count('id') ,min_price= Min('price'))
+    queryset = Customer.objects.annotate(is_new=Value(True))
+
+    return render(request, 'hello.html', {'name': 'Nour', 'result': list(queryset)})
